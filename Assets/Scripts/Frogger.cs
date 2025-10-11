fe495538b1345ec3ee19819c7784e70e13cad227
yingersss <vangyy14@gmail.com>
2025-10-10 23:11:12 -0500

    frog leap animation

--- Assets/Scripts/Frogger.cs
+++ Assets/Scripts/Frogger.cs
@@ -1,8 +1,18 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Frogger : MonoBehaviour
 {
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite idleSprite;
+    public Sprite leapSprite;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -37,11 +47,30 @@ public class Frogger : MonoBehaviour
             Console.WriteLine("D key was pressed");
         }
     }
-    
+
     private void Move(Vector3 direction)
     {
-        transform.position += direction;
+        Vector3 destination = transform.position + direction;
+        StartCoroutine(Leap(destination));
     }
     
+    private IEnumerator Leap(Vector3 destination) // Coroutine to move the frog smoothly between start and destination points
+    {
+        spriteRenderer.sprite = leapSprite; // change sprite to leap sprite when frog is moving
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0;
+        float duration = 0.125f; // 125 milliseconds
 
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+            elapsedTime += Time.deltaTime; // elapsed time is added
+            yield return null;
+        }
+
+        transform.position = destination; // final position is set to the destination so where frog will end up
+        spriteRenderer.sprite = idleSprite;
+
+	}
 }
